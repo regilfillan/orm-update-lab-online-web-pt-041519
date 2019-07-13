7b4f6511694be9ba1e446022865246a237d80278
Ryan Gilfillan <regilfillan@gmail.com>
2019-07-13 22:26:13 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -10,7 +10,13 @@ class Student
   end 
   
   def self.create_table
-    
+    CREATE TABLE IF NOT EXISTS songs (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        album TEXT
+        )
+        SQL
+    DB[:conn].execute(sql)
   end
 
 end
